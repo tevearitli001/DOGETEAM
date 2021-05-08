83634a1c922ecda69124c60c0b3f14a803c45c45
fjs57 <fjs57500@gmail.com>
2021-05-08 17:57:53 +0200

    Add files via upload

--- ML_fraud_PoC.py
+++ ML_fraud_PoC.py
@@ -1,15 +1,21 @@
+##############################################
+#           Imports de modules               #
+##############################################
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-#import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
 
-data = np.loadtxt('ML_output.csv', delimiter=",", dtype=np.float32, skiprows=1)
+##############################################
+#                 Réglages                   #
+##############################################
+
+data = np.loadtxt('output.csv', delimiter=",", dtype=np.float32, skiprows=1)
 
 input_params = 5
 hidder_layer = 15
@@ -17,6 +23,10 @@ outputs = 1
 
 max_epochs = 10000
 
+
+##############################################
+#         Variables du programme             #
+##############################################
 
 data_tensor = torch.tensor(data)
 
@@ -27,6 +37,10 @@ y_train = train_set[:,-1].view(train_set.shape[0], -1)
 test_set = data_tensor[150:]
 x_test = test_set[:,:-1]
 y_test = test_set[:,-1].view(test_set.shape[0], -1)
+
+##############################################
+#           Structure du réseau              #
+##############################################
 
 class Net(nn.Module):
 
@@ -50,26 +64,43 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+##############################################
+#              Entrainement                  #
+##############################################
+
+# instanciation du réseau de neurones
 model = Net(input_params, hidder_layer, outputs)
 
+# critère d'optimisation
 criterion = torch.nn.MSELoss()
+
+# optimisateur
 optimizer = optim.SGD(model.parameters(), lr=1e-2)
 
+# passage en mode entrainement
 model.train()
+
+# répétition de l'entrainement 
 for t in range(max_epochs):
+
+    #estimation des risques de fraudes
     y_pred = model(x_train)
 
+    # calcul de "l'erreur"
     loss = criterion(y_pred, y_train.reshape(-1,1))
+
+
     if t % 100 == 99:
         print(t, loss.item())
 
+    #reset de l'erreur et propagation des corrections
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
 #print((y_pred>0.5)==y_train )
 
+
+#estimation de l'erreur sur la base de test
 y_pt = model(x_test)
-
-
 print(float(sum((y_pt>0.5) == y_test)/test_set.shape[0]*100),"% de succès")
