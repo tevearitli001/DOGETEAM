dba7edd7ffdfa01d4c8807d1276ff213aefab6c8
Nicolas Bouron <tevearitli001>
2021-05-08 17:27:44 +0200

    code by default

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+from qiskit import Aer, transpile, assemble
+from qiskit.circuit.library import QFT
+from qiskit.aqua import QuantumInstance, aqua_globals
+from qiskit.quantum_info import state_fidelity
+from qiskit.aqua.algorithms import HHL, NumPyLSsolver
+from qiskit.aqua.components.eigs import EigsQPE
+from qiskit.aqua.components.reciprocals import LookupRotation
+from qiskit.aqua.operators import MatrixOperator
+from qiskit.aqua.components.initial_states import Custom
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def create_eigs(matrix, num_auxiliary, num_time_slices, negative_evals):
+    ne_qfts = [None, None]
+    if negative_evals:
+        num_auxiliary += 1
+        ne_qfts = [QFT(num_auxiliary - 1), QFT(num_auxiliary - 1).inverse()]
+
+    return EigsQPE(MatrixOperator(matrix=matrix),
+                   QFT(num_auxiliary).inverse(),
+                   num_time_slices=num_time_slices,
+                   num_ancillae=num_auxiliary,
+                   expansion_mode='suzuki',
+                   expansion_order=2,
+                   evo_time=None,  # This is t, can set to: np.pi*3/4
+                   negative_evals=negative_evals,
+                   ne_qfts=ne_qfts)
+
+def fidelity(hhl, ref):
+    solution_hhl_normed = hhl / np.linalg.norm(hhl)
+    solution_ref_normed = ref / np.linalg.norm(ref)
+    fidelity = state_fidelity(solution_hhl_normed, solution_ref_normed)
+    print("Fidelity:\t\t %f" % fidelity)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+matrix = [[1, -(1/3)], [-(1/3), 1]] # [[1, -1/3], [-1/3, 1]]
+vector = [1, 0] # [1, 0]
 
+orig_size = len(vector)
+matrix, vector, truncate_powerdim, truncate_hermitian = HHL.matrix_resize(matrix, vector)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Initialize eigenvalue finding module
+eigs = create_eigs(matrix, 3, 50, False)
+num_q, num_a = eigs.get_register_sizes()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Initialize initial state module
+init_state = Custom(num_q, state_vector=vector)
+
+# Initialize reciprocal rotation module
+reci = LookupRotation(negative_evals=eigs._negative_evals, evo_time=eigs._evo_time)
+
+algo = HHL(matrix, vector, truncate_powerdim, truncate_hermitian, eigs,
+           init_state, reci, num_q, num_a, orig_size)
+
+result = algo.run(QuantumInstance(Aer.get_backend('statevector_simulator')))
+print("Solution:\t\t", np.round(result['solution'], 5))
+
+result_ref = NumPyLSsolver(matrix, vector).run()
+print("Classical Solution:\t", np.round(result_ref['solution'], 5))
+
+print("Probability:\t\t %f" % result['probability_result'])
+fidelity(result['solution'], result_ref['solution'])
+
+print("circuit_width:\t", result['circuit_info']['width'])
+print("circuit_depth:\t", result['circuit_info']['depth'])
+print("CNOT gates:\t", result['circuit_info']['operations']['cx'])
+
+qobj = assemble(qcs, shots=10)
